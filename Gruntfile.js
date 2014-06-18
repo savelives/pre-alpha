9842f59316049d17e06e172bf69c3b23cdd3a1ff
Lucas Churchill <lucas@agtlucas.com>
2014-06-18 00:04:27 -0300

    Removing Require.js from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,9 @@ module.exports = function (grunt) {
     });
 
     // Register group tasks
-    grunt.registerTask('build', [ 'jshint', 'compass', 'requirejs', 'i18n', 'copyto' ]);
+    grunt.registerTask('build', [ 'jshint', 'compass', 'i18n', 'copyto' ]);
 
-    grunt.registerTask('w', ['watch', 'compass', 'requirejs']);
+    grunt.registerTask('w', ['watch', 'compass']);
 
     grunt.registerTask('test', [ 'jshint', 'mochacli' ]);
 
