4c3184bff63095e48f1e8f0b5052ce46b3c49736
Lucas Churchill <lucas@agtlucas.com>
2014-06-20 02:38:10 -0300

    Switch tabs method

--- public/build/js/bundle.js
+++ public/build/js/bundle.js
@@ -9212,11 +9212,12 @@ var tabs = (function (window, document, undefined) {
   };
 
   tabs.switchTabs = function () {
-    var tabsContent = $('.tabs-content > section');
-    var idx = $('.can-cant li').data('index');
+    var tabsContent = $('.tabs-content section');
     $('.can-cant li').on('click', function (e) {
       e.preventDefault();
       $(this).addClass('tab-current').siblings().removeClass('tab-current');
+      tabsContent.removeClass('tab-content-current');
+      $(tabsContent[$(this).data('index') - 1]).addClass('tab-content-current');
     });
   };
 
